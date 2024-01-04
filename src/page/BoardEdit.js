0449269e8bc0600e078885f5a9a72ea45e4a606e
이정훈 <gns14585@naver.com>
2024-01-05 01:24:47 +0900

    상품 수정 - 이미지 수정완료

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -1,16 +1,21 @@
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormLabel,
+  Image,
   Input,
   Spinner,
+  Switch,
   Textarea,
   useToast,
 } from "@chakra-ui/react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 export function BoardEdit() {
   const navigate = useNavigate();
@@ -19,6 +24,9 @@ export function BoardEdit() {
   const { id } = useParams();
 
   const toast = useToast();
+
+  const [removeMainImgs, setRemoveMainImgs] = useState([]);
+  const [mainImg, setMainImg] = useState(null);
 
   // ------------------------------ 상품 클릭시 상품 렌더링 ------------------------------
   useEffect(() => {
@@ -34,7 +42,14 @@ export function BoardEdit() {
   // ------------------------------ 수정버튼 클릭 로직 ------------------------------
   function handleUpdate() {
     axios
-      .put("/api/board/edit", board)
+      .putForm("/api/board/edit", {
+        id: board.id,
+        title: board.title,
+        content: board.content,
+        price: board.price,
+        removeMainImgs,
+        mainImg,
+      })
       .then(() => {
         toast({
           description: id + "번 상품 수정 되었습니다.",
@@ -48,6 +63,16 @@ export function BoardEdit() {
           status: "error",
         });
       });
+  }
+
+  function handleRemoveMainImgSwitch(e) {
+    if (e.target.checked) {
+      setRemoveMainImgs([...removeMainImgs, e.target.value]);
+    } else {
+      setRemoveMainImgs(
+        removeMainImgs.filter((item) => item !== e.target.value),
+      );
+    }
   }
 
   return (
@@ -75,6 +100,39 @@ export function BoardEdit() {
         <Input
           value={board.price}
           onChange={(e) => setBoard({ ...board, price: e.target.value })}
+        />
+      </FormControl>
+
+      <Flex>
+        {/* 이미지 출력 */}
+        {board.mainImgs.length > 0 &&
+          board.mainImgs.map((mainImg) => (
+            <Box key={mainImg.id} my="5px">
+              <FormControl display="flex" alignItems="center">
+                <FormLabel>
+                  <FontAwesomeIcon color="red" icon={faTrashCan} />
+                </FormLabel>
+                <Switch
+                  value={mainImg.id}
+                  colorScheme="red"
+                  onChange={handleRemoveMainImgSwitch}
+                />
+              </FormControl>
+
+              <Box>
+                <Image src={mainImg.url} alt={mainImg.name} w={"150px"} />
+              </Box>
+            </Box>
+          ))}
+      </Flex>
+
+      <FormControl>
+        <FormLabel>메인 이미지</FormLabel>
+        <Input
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={(e) => setMainImg(e.target.files)}
         />
       </FormControl>
 
